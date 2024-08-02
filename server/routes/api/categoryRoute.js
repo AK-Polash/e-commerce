acd3afd39ab2ca89b270855f87d144f0faeac278
AK-Polash <polashk199@gmail.com>
2024-08-03 04:13:38 +0600

    Category route has been added here

--- server/routes/api/categoryRoute.js
+++ server/routes/api/categoryRoute.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const {
   createCategoryController,
+  getAllCategoryController,
+  getCategoryController,
+  updateCategoryController,
+  deleteCategoryController,
 } = require("../../controllers/categoryController");
 
 const router = express.Router();
 
-router.route("/").post(createCategoryController);
+router.route("/").post(createCategoryController).get(getAllCategoryController);
+
+router
+  .route("/:id")
+  .get(getCategoryController)
+  .patch(updateCategoryController)
+  .delete(deleteCategoryController);
 
 module.exports = router;
